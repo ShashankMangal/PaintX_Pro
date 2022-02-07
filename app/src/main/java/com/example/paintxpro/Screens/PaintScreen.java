d646d485c258e9c4023c68153845b370b5638fa7
ShashankMangal <shashankmangal10@gmail.com>
2022-02-07 12:55:06 +0530

    paint screen ready

--- app/src/main/java/com/example/paintxpro/Screens/PaintScreen.java
+++ app/src/main/java/com/example/paintxpro/Screens/PaintScreen.java
@@ -1,16 +1,74 @@
 package com.example.paintxpro.Screens;
 
+import static com.example.paintxpro.Screens.display.colorList;
+import static com.example.paintxpro.Screens.display.current_brush;
+import static com.example.paintxpro.Screens.display.pathList;
+
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Path;
 import android.os.Bundle;
+import android.view.View;
 
 import com.example.paintxpro.R;
 
 public class PaintScreen extends AppCompatActivity {
+
+    public static Path path = new Path();
+    public static Paint paint_brush = new Paint();
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_paint_screen);
     }
+
+    public void pencil(View view) {
+        paint_brush.setColor(Color.BLACK);
+        currentColor(paint_brush.getColor());
+
+    }
+
+    public void redColor(View view) {
+        paint_brush.setColor(Color.RED);
+        currentColor(paint_brush.getColor());
+    }
+
+    public void yellowColor(View view) {
+        paint_brush.setColor(Color.YELLOW);
+        currentColor(paint_brush.getColor());
+    }
+
+    public void greenColor(View view) {
+        paint_brush.setColor(Color.GREEN);
+        currentColor(paint_brush.getColor());
+    }
+
+    public void blueColor(View view) {
+        paint_brush.setColor(Color.BLUE);
+        currentColor(paint_brush.getColor());
+    }
+
+    public void blackColor(View view) {
+        paint_brush.setColor(Color.BLACK);
+        currentColor(paint_brush.getColor());
+    }
+
+    public void eraser(View view) {
+        pathList.clear();
+        colorList.clear();
+        path.reset();
+    }
+
+
+    public void currentColor(int c)
+    {
+        current_brush = c;
+        path = new Path();
+    }
+
 }
