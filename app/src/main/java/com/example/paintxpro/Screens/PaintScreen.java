26c7894fdffb943ccccf059855b07e63cd1936f6
ShashankMangal <shashankmangal10@gmail.com>
2022-02-07 13:23:33 +0530

    menu added

--- app/src/main/java/com/example/paintxpro/Screens/PaintScreen.java
+++ app/src/main/java/com/example/paintxpro/Screens/PaintScreen.java
@@ -4,13 +4,20 @@ import static com.example.paintxpro.Screens.display.colorList;
 import static com.example.paintxpro.Screens.display.current_brush;
 import static com.example.paintxpro.Screens.display.pathList;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.Path;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.Toast;
 
 import com.example.paintxpro.R;
 
@@ -71,4 +78,57 @@ public class PaintScreen extends AppCompatActivity {
         path = new Path();
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main_menu, menu);
+
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+
+        switch (item.getItemId())
+        {
+            case R.id.rateUs:
+                Uri uri = Uri.parse("https://play.google.com/store/apps/details?id=" + getPackageName());
+                Intent i = new Intent(Intent.ACTION_VIEW, uri);
+
+                try {
+                    startActivity(i);
+                } catch (Exception e) {
+
+                    Toast.makeText(getApplicationContext(), "Error :" + e.getMessage(), Toast.LENGTH_SHORT).show();
+                }
+
+                return true;
+
+            case R.id.update:
+                Uri uri2 = Uri.parse("https://play.google.com/store/apps/details?id=" + getPackageName());
+                Intent i2 = new Intent(Intent.ACTION_VIEW, uri2);
+
+                try {
+                    startActivity(i2);
+                } catch (Exception e) {
+
+                    Toast.makeText(getApplicationContext(), "Error :" + e.getMessage(), Toast.LENGTH_SHORT).show();
+                }
+
+                return true;
+
+            case R.id.privacy:
+                Toast.makeText(this, "Privacy", Toast.LENGTH_SHORT).show();
+                return true;
+
+            case R.id.terms:
+                Toast.makeText(this, "Terms", Toast.LENGTH_SHORT).show();
+                return true;
+
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+
+    }
 }
