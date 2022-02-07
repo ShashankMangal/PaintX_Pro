b5100f39f1177b308c014b4b3c879e5a0c34ce0f
ShashankMangal <shashankmangal10@gmail.com>
2022-02-07 11:43:40 +0530

    Initial commit

---
+++ app/src/main/java/com/example/paintxpro/Screens/PaintScreen.java
@@ -0,0 +1,16 @@
+package com.example.paintxpro.Screens;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.paintxpro.R;
+
+public class PaintScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_paint_screen);
+    }
+}
